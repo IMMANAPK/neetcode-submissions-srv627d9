5d7f530e7b59022a5f28876115c5c0593e55c91f
immanuvel C <88032392+IMMANAPK@users.noreply.github.com>
2026-04-15 17:31:06 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        let set = new Set(nums);
+        if(nums.length === set.size){
+            return false
+        }else{
+            return true
+        }
+
+    }
+}
